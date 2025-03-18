d38efe4947e53e976d9d318c3acd8842fa0ef40f
kawacatoose <heerovanderveen@outlook.com>
2025-03-18 20:10:51 +0100

    Added player.py

--- main.py
+++ main.py
@@ -3,10 +3,12 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
@@ -21,6 +23,7 @@ def main():
 				return    			
 			
 		screen.fill(("black"))
+		player.draw(screen)
 		pygame.display.flip()
 
 		dt = clock.tick(60) / 1000
